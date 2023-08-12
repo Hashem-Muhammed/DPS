08fcc0d5500bfcfc51b9e6629c7a2cdd7984dde7
Hashem Muhammed <hashem.muhammed@microengineering.ca>
2023-08-12 23:00:56 +0300

    Image and Document models added

--- dp_controller/models.py
+++ dp_controller/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Image(models.Model):
+    image = models.ImageField(upload_to="images/")
+    wdith = models.IntegerField()
+    height = models.IntegerField()
+    number_of_channels = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.id}"
+
+
+class Document(models.Model):
+    document = models.FileField(upload_to="documents/")
+    number_of_pages = models.IntegerField()
+    page_width = models.IntegerField()
+    page_height = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.id}"
