c9a13f3c9e8dc98b654209e963112999fae8d64a
Hashem Muhammed <hashem.muhammed@microengineering.ca>
2023-08-13 01:54:53 +0300

    images and documents viewsets

--- dp_controller/urls.py
+++ dp_controller/urls.py
@@ -1,5 +1,14 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+router = DefaultRouter()
 from .views import *
+
+router.register(r'images', ImagesViewSet, basename='images')
+router.register(r'pdfs', DocumentsViewSet, basename='pdfs')
+
 urlpatterns = [
     path('upload/', view = upload_file ),
+    path('', include(router.urls)),
+
 ]
+ 
--- dp_controller/views.py
+++ dp_controller/views.py
@@ -4,8 +4,8 @@ from .serializers import ImageSerializer, DocumentSerializer
 from rest_framework.response import Response
 from rest_framework import status
 from .services import b64_to_pdf
-
-
+from rest_framework import viewsets
+from .models import *
 # Create your views here.
 @api_view(["POST"])
 def upload_file(request):
@@ -22,3 +22,13 @@ def upload_file(request):
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class ImagesViewSet(viewsets.ModelViewSet):
+    queryset = Image.objects.all()
+    serializer_class = ImageSerializer
+
+class DocumentsViewSet(viewsets.ModelViewSet):
+    queryset = Document.objects.all()
+    serializer_class = DocumentSerializer
+
